0e0a2c0ee47cdaaf3cd289ca1a3f0a60c4d70811
NickBustos <nicolasnahuelb@alumnos.iesgalileo.es>
2022-10-04 19:02:24 +0200

    Proyecto V.01
    Estructura general del proyecto.

---
+++ index.php
@@ -0,0 +1,31 @@
+<?php include "templates/cabecera.php" ?>
+
+<div class="container">
+
+    <div class="row">
+        <div class="card-columns">
+            <div class="card">
+                <img class="card-img-top" src="holder.js/100x180/" alt="">
+                <div class="card-body">
+                
+                <!-- INSERTE FORMULARIO DE REGISTRO AQUÍ --> 
+
+                </div>
+            </div>
+
+
+
+            <div class="card">
+                <img class="card-img-top" src="holder.js/100x180/" alt="">
+                <div class="card-body">
+
+                <!-- INSERTE FORMULARIO DE INICIO DE SESIÓN AQUÍ -->
+
+
+                </div>
+            </div>
+        </div>
+
+    </div>
+</div>
+<?php include "templates/pie.php" ?>
