666ea4c1b27deee0a555531835c7991b16158655
mmontalzuelo <m.montalzuelo@gmail.com>
2022-10-06 19:06:39 +0200

    hola
    a

--- templates/cabecera.php
+++ templates/cabecera.php
@@ -1,16 +1,18 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Trabajo</title>
-    <link rel="stylesheet" href="./css/archivo.css"/>
-    <link rel="stylesheet" href="./css/style.css"/>
-    
+    <link rel="stylesheet" href="./css/archivo.css" />
+    <link rel="stylesheet" href="./css/style.css" />
+
 </head>
+
 <body>
-<nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+    <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
         <ul class="nav navbar-nav">
             <li class="nav-item active">
                 <a class="nav-link" href="">Inicio</a>
@@ -26,30 +28,34 @@
         </ul>
     </nav>
     <div class="container">
-    <br></br>
+        <br></br>
 
     </div>
 
 
-    <?php 
-    $_nombreUsuario="";
+    <?php
+    $_nombreUsuario = "";
 
-    if(!empty($_POST)){
-        include 'config.php';
-        include 'funciones.php';
-        $nombreUserPass = "";
-        $_nombreUsuario = htmlspecialchars($_POST["nombreDeUsuario"]); 
-            if(!empty($nombreDeUsuario)){
-                if(!validar($nombreDeUsuario, VALIDA_NOMBREUSUARIO)){
-                    echo "Por favor, ingrese un nombre válido";
-
-                }else{
-                    $nombreUserPass = $_nombreUsuario;
-                }
-            }else{
-            echo "el campo de nombre no puede estar vacío";
+    if (!empty($_POST)) {
+        include 'config.php'; //creo que seria así = ../Configuraciones.config.php;
+        include 'funciones.php'; //creo que seria así = ../Configuraciones.funciones.php;
+        $nombreUserPass = $avatar = "";
+        $_nombreUsuario = htmlspecialchars($_POST["nombreDeUsuario"]);
+        if (!empty($nombreDeUsuario)) {
+            if (!validar($nombreDeUsuario, VALIDA_NOMBREUSUARIO)) {
+                echo "Por favor, ingrese un nombre válido";
+            } else {
+                $nombreUserPass = $_nombreUsuario;
             }
-   
+        } else {
+            echo "el campo de nombre no puede estar vacío";
+        }
+        if(empty($_FILES) == false && empty($_FILES["avatar"]) == false){
+            $avatar = getImage($_FILES["avatar"]);
+            //saveImage($_FILES["avatar"]);
+        }else{
+            echo "introduce una imagen .png";
+        }
     }
 
     ?>
