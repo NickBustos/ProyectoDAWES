20c651f2516251b173732677c633223f31325ebc
mmontalzuelo <m.montalzuelo@gmail.com>
2022-10-06 17:57:47 +0200

    he juntado funciones en funciones
    a lo mejor se rompe todo

--- Configuraciones/config.php
+++ Configuraciones/config.php
@@ -1,3 +1,5 @@
 <?php
 define("VALIDA_NOMBREUSUARIO", "/([A-ZÁ-Ú]{1}[-a-zá-ú]+\s?)+/A");
+define("PATH_TO_IMAGES", '../imagenes/');
+define("PATH_TO_BD", "bbdd.txt");
 ?>
--- Configuraciones/funciones.php
+++ Configuraciones/funciones.php
@@ -38,4 +38,96 @@ function validarEdad($fecha){
     }
 }
 
+/**
+ * Recoge imagen de un file
+ * @param $_FILES["nombreFile"]
+ * @return imagen
+ */
+function getImage($file){
+    if(empty($file) == false){
+        return "data:" . $file["type"] . ";base64," . 
+        base64_encode(file_get_contents($file["tmp_name"]));
+    }
+}
+
+/**
+ * Cuenta numero de imagenes.png en carpeta files
+ */
+function getNumberOfLogo(){
+    return count(glob(PATH_TO_IMAGES . '{*.png}', GLOB_BRACE));
+}
+
+/**
+ * Guardar imagen en carpeta files
+ */
+function saveImage($file){
+    if(empty($file) == false){
+        if(is_uploaded_file($file["tmp_name"])){
+            //CREAR CAMINO
+            $finalpath= PATH_TO_IMAGES . getNumberOfLogo() . ".png";//se puede cambiar y sitio
+            return (move_uploaded_file($file["tmp_name"], $finalpath));
+            //GUARDAR
+        }
+    }
+}
+
+/**
+ * Recorrer un file un txt
+ * @param path con nombre del archivo
+ */
+function recorrer($txt=PATH_TO_BD){
+    $contadorlinea=0;
+    $lineas=[];
+    $fichero = fopen($txt, "r");
+    while(!feof($fichero)){
+        $linea = "";
+        $linea = fgets($fichero);
+        $lineas[$contadorlinea++]=$linea;   
+    }
+    return $lineas;
+}
+
+/**
+ * Coger usuario de una línea de recorrer
+ */
+function getUser($linea){
+    return explode(';', $linea)[0];
+}
+
+/**
+ * Coger password de una línea de recorrer
+ */
+function getPassword($linea){
+    return explode(';', $linea)[1];
+}
+
+/**
+ * Averigua si un nombre de usuario esta usado en file bbdd
+ */
+function isUsed($user){
+    $lineas = recorrer(PATH_TO_BD);
+    
+    for($linea = 0; $linea < sizeof($lineas); $linea++){
+        if($user == getUser($lineas[$linea])){
+            return true;//1
+        }
+    }
+    return false;//0
+}
+
+/**
+ * Verifica que un usuario y contraseña se encuentran en file bbdd
+ */
+function login($user, $password) {
+    $lineas = recorrer(PATH_TO_BD);
+    for($linea = 0; $linea < sizeof($lineas); $linea++){
+        if($user == getUser($lineas[$linea])
+            && $password == getPassword($lineas[$linea])){
+            return true;//1
+            //se puede cambiar
+        }
+    }
+    return false;//0
+}
+
 ?>
