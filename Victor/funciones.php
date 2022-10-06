99d6cebfb1cdbfdbd480d2bb09b1e944ae98b4b6
VictorPueGar <victorpg@alumnos.iesgalileo.es>
2022-10-06 15:52:18 +0200

    ValidarFecha

--- Victor/funciones.php
+++ Victor/funciones.php
@@ -1,20 +1,20 @@
 <?php
 
-function validarRegistro($campo,$patron){
-    if(preg_match($campo,$patron)){
-        return true;
-    }
-    return false;
-}
 
-
-function validarEdad($fecha){
-    $hoy = new DateTime();
-    if($hoy -> diff($fecha) > 18){
+function calculaedad($fechanacimiento){
+    list($ano,$mes,$dia) = explode("-",$fechanacimiento);
+    $ano_diferencia  = date("Y") - $ano;
+    $mes_diferencia = date("m") - $mes;
+    $dia_diferencia   = date("d") - $dia;
+    if ($dia_diferencia < 0 || $mes_diferencia < 0)
+      $ano_diferencia--;
+    if($ano_diferencia >= 18){
         return true;
     }else{
         return false;
     }
-}
+  }
+
+
 
 ?>
