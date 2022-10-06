d48236aa469a0410d2b22df3579e8122ef3b9e7f
NickBustos <nicolasnahuelb@alumnos.iesgalileo.es>
2022-10-06 17:57:10 +0200

    Proyecto V0.15
    Validación de nombre semi terminada.

--- templates/cabecera.php
+++ templates/cabecera.php
@@ -35,9 +35,20 @@
     $_nombreUsuario="";
 
     if(!empty($_POST)){
+        include 'config.php';
         include 'funciones.php';
+        $nombreUserPass = "";
         $_nombreUsuario = htmlspecialchars($_POST["nombreDeUsuario"]); 
-        
+            if(!empty($nombreDeUsuario)){
+                if(!validar($nombreDeUsuario, VALIDA_NOMBREUSUARIO)){
+                    echo "Por favor, ingrese un nombre válido";
+
+                }else{
+                    $nombreUserPass = $_nombreUsuario;
+                }
+            }else{
+            echo "el campo de nombre no puede estar vacío";
+            }
    
     }
 
