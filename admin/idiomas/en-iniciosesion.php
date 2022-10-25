c33fdecec1f414d5a1b45a391fdd7ee0919eec68
mmontalzuelo <m.montalzuelo@gmail.com>
2022-10-25 16:13:42 +0200

    enlace mal

--- admin/idiomas/en-iniciosesion.php
+++ admin/idiomas/en-iniciosesion.php
@@ -4,7 +4,7 @@
     "iniciosesion" => "Sing in",
     "username" => "Username:",
     "password" => "Password:",
-    "noregistrado" => "¿You doesn't have an account? Click <a href='iniciosesion.php'>here</a>",
+    "noregistrado" => "¿You doesn't have an account? Click <a href='registrarse.php'>here</a>",
     "error_vacio"=> ERROR_IN . "The field can not be empty" . ERROR_OUT,
     "error_login_user"=> ERROR_IN . "Unrecognized user" . ERROR_OUT,
     "error_login_pass"=> ERROR_IN . "Incorrect password" . ERROR_OUT
