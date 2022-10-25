5fffee82d499e11b5d64ed15e09ddfbd84e3a012
mmontalzuelo <m.montalzuelo@gmail.com>
2022-10-25 17:23:56 +0200

    Cambiar idioma

--- cambiarIdioma.php
+++ cambiarIdioma.php
@@ -1,23 +1,13 @@
 <?php
-include "admin/templates/cabecera.php";
-
 if (isset($_COOKIE["lang"])) {
-
-    if($_COOKIE["lang"] == "es"){
-
+    if($_COOKIE["lang"] === "es"){
         setCookie("lang","en",time()+60);
-
     }else{
-
         setCookie("lang","es",time()+60);
-
     }
-
     }else{
         setCookie("lang","es",time()+60);
-
 }
-
-header('Location: index.php');
+header('Location: ' . $_SERVER["HTTP_REFERER"]);
 exit();
 ?>
