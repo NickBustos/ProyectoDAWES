e12d11ceec59402754ccfbdb64ed4be9b421f65f
VictorPueGar <victorpg@alumnos.iesgalileo.es>
2022-10-25 16:07:41 +0200

    Arreglado el link
    Link arreglado (en inicio sesion no podiamos ir a registrarnos si le daba el usuario)

--- admin/idiomas/es-iniciosesion.php
+++ admin/idiomas/es-iniciosesion.php
@@ -4,7 +4,7 @@
     "iniciosesion" => "Iniciar sesión",
     "username" => "Nombre:",
     "password" => "Contraseña:",
-    "noregistrado" => "¿No tienes cuenta? Haz clíck <a href='iniciosesion.php'>aquí</a>",
+    "noregistrado" => "¿No tienes cuenta? Haz clíck <a href='registrarse.php'>aquí</a>",
     "error_vacio"=> ERROR_IN . "El campo no puede estar vacío" . ERROR_OUT,
     "error_login_user"=> ERROR_IN . "El nombre no existe" . ERROR_OUT,
     "error_login_pass"=> ERROR_IN . "La contraseña no es correcta" . ERROR_OUT
