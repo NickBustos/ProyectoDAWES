3902b1c5638d38e71f175533721eb64b71b64e79
VictorPueGar <victorpg@alumnos.iesgalileo.es>
2022-11-23 18:11:48 +0100

    dsfjkds

---
+++ admin/configuraciones/configDB.php
@@ -0,0 +1,11 @@
+<?php
+//Definir constantes relacionadas con la BD
+define("HOST","localhost");
+define("user","root");
+define("PASSWORD","");
+define("DB","bd"); //Cambiar esto
+define("DRIVER","mysql");
+define ("DSN", DRIVER. ":host=" . HOST . ";dbname=" .DB);
+
+
+?>
--- admin/configuraciones/funciones.php
+++ admin/configuraciones/funciones.php
@@ -1,5 +1,6 @@
 <?php
 include 'config.php';
+include 'configDB.php';
 
 //--------------------------------- OPERACIONES BBDD --------------------------------
 /**
@@ -104,4 +105,16 @@ function iniciarSesion($linea)
     $_SESSION[SESSION_MAIL] = getDato(LINE_MAIL, $linea);
     $_SESSION[SESSION_DATE] = getDato(LINE_DATE, $linea);
     $_SESSION[SESSION_FILE] = getDato(LINE_FILE, $linea);
+}
+
+//----------------------------- Función para registrar los datos en la BD sin el modo Visualización y sin Idioma-----------------------------
+$conexion_db = new PDO($DSN,user,PASSWORD);
+
+function insertarCliente($fechaNac, $image, $email, $rol){
+
+    if(isset($conexion_db)){
+        $sql = "INSERT INTO 'USUARIO' ('FechaNacimiento','FotoURL', 'Email', 'Rol' VALUES ('[$fechaNac]','[$image]','[$email]','[$rol]')";
+        $pdostmt = $conexion_db->prepare(($sql));
+        $pdostmt->execute();
+
 }
