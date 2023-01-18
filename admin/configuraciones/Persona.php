e4d65a781cfed6c70339e8cbd3c60c212d071da8
NickBustos <nicolasnahuelb@alumnos.iesgalileo.es>
2023-01-18 20:59:45 +0100

    CLASE PERSONA
    Comienzo a crear la clase persona

---
+++ admin/configuraciones/Persona.php
@@ -0,0 +1,44 @@
+<?php
+
+class Persona
+{
+    public $perfilId = '';
+    public $perfilFoto = '';
+    public $perfilBatallasCreadas = '';
+    public $perfilVotos = '';
+    public $perfilDenuncias = '';
+
+    /*function __construct(int $perfilId, int $perfilFoto, int $perfil)
+    {
+        
+    }*/
+
+    public function selectFromUsuario($campos)
+    {
+        $conexion = new PDO(DSN, USER, PASSWORD);
+        $sql = "SELECT ";
+        for ($i = 0; $i < count($campos); $i++) {
+            $sql .= "{$campos[$i]}";
+            if ($i < count($campos) - 1) {
+                $sql .= ",";
+            }
+            $sql .= " ";
+        }
+        $sql .= " FROM usuario WHERE id='" . $_SESSION[SESSION_ID] . "'";
+        $resultado = $conexion->query($sql);
+        $registro = $resultado->fetch(PDO::FETCH_NUM);
+        return $registro;
+    }
+
+    public function existe($user)
+    {
+        $conexion = new PDO(DSN, USER, PASSWORD);
+        $sql = "SELECT password FROM credencial WHERE nombreusuario = '{$user}'";
+        $resultado = $conexion->query($sql);
+        // return count($resultado->fetchAll(PDO::FETCH_NUM));
+        if ($linea = $resultado->fetch(PDO::FETCH_NUM)) {
+            return $linea[0];
+        }
+        return false;
+    }
+}
